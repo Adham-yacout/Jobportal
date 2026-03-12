90a1df2482277b867cde39f4fb4616716f6651de
adham yacout <adham.yacout@gmail.com>
2026-03-12 14:52:32 +0300

    + Authorization for data in databse

--- src/main/java/com/example/jobHunter/job/repository/JobPortalUserRepository.java
+++ src/main/java/com/example/jobHunter/job/repository/JobPortalUserRepository.java
@@ -3,5 +3,11 @@ package com.example.jobHunter.job.repository;
 import com.example.jobHunter.Entity.JobPortalUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JobPortalUserRepository extends JpaRepository<JobPortalUser,Long> {
+
+
+    Optional<JobPortalUser> findJobPortalUserByEmail(String email);
+
 }
