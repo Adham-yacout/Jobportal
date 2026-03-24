e039ceed387b6943c1268fe67c20b6bf927d609c
adham yacout <adham.yacout@gmail.com>
2026-03-24 13:47:59 +0300

    + Authorization for data in databse

--- src/main/java/com/example/jobHunter/company/controller/CompanyController.java
+++ src/main/java/com/example/jobHunter/company/controller/CompanyController.java
@@ -4,6 +4,7 @@ package com.example.jobHunter.company.controller;
 import com.example.jobHunter.dto.CompanyDto;
 import com.example.jobHunter.company.service.ICompanyService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +15,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/companies")
 @RequiredArgsConstructor
+@Slf4j
 public class CompanyController {
 
     private final ICompanyService companyService;
 
     @GetMapping
     public ResponseEntity<List<CompanyDto>> getAllCompanies() {
+        log.info("get all companies");
         List<CompanyDto> companyList = companyService.getAllCompanies();
         return ResponseEntity.ok().body(companyList);
     }
